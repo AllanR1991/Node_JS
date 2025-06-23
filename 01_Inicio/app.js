606cacd684d7b2d202e8dbb41a13935cdf309d3f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-22 22:45:46 -0300

    feat: adiciona console.log e lógica If

--- 01_Inicio/app.js
+++ 01_Inicio/app.js
@@ -1,1 +1,13 @@
+var mostrarSite = true;
+
+const site= "www.guiadoprogramador.com";
+
 console.log("Hello World!");
+
+console.log("Meu nome é Allan");
+
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if(mostrarSite){
+    console.log(site)
+}
