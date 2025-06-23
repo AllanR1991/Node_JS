1c3f659beec07c8ce7af97f76a3cf137419b426d
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-23 00:16:19 -0300

    feat: incluindo html na resposta do servidor.

--- 02-HTTP/app.js
+++ 02-HTTP/app.js
@@ -1,7 +1,7 @@
 var http = require("http"); //O Modulo http ja vem com o node.
 
 http.createServer(function(requisição, resposta) {
-    resposta.end("Bem vindo ao meu site");
+    resposta.end("<h1>Bem vindo ao meu site</h1><br><h4>guiadoprogramador.com</h4>");
 }).listen(8080);// Criando um servidor com node.
 
 console.log("Meu servidor está rodando!");
