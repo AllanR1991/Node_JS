7288f936e17501b44767cae922591b633bc9567a
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-23 22:54:57 -0300

    feat: criado a primeira rota, poreém sem retorno **Tomar cuidado.

--- 03-Express/index.js
+++ 03-Express/index.js
@@ -2,6 +2,13 @@ const express = require("express"); //importando o express para nosso projeto.
 
 const app = express(); // iniciando o express.
 
+
+//Criando rota para evitar a mensagem de "cannot get" ao rodar o servidor
+app.get("/",function(req, res){
+    
+});
+
+
 app.listen(4000,function(erro){
     if(erro){
         console.log("Ocorreu um erro!")
