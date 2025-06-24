c258643e70472f561456d86fb453240ef4743f05
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-24 08:03:28 -0300

    feat: adicionando novas rotas a "blog" e "canal/youtube"

--- 03-Express/index.js
+++ 03-Express/index.js
@@ -3,11 +3,23 @@ const express = require("express"); //importando o express para nosso projeto.
 const app = express(); // iniciando o express.
 
 
-//Criando rota para evitar a mensagem de "cannot get" ao rodar o servidor
+//Criando rota para evitar a mensagem de "cannot get" ao rodar o servidor, devolvendo uma respopsta para a rota
 app.get("/",function(req, res){
-    
+    //Cada rota deve ter apenas uma rota
+        
+    res.send("Bem vindo ao guia do programador")//Resposta da pagina pode ser um arquivo um html entre outros
+
+    //Após ja ter enviado a respota para rota é como se a conexão estivese sido encerrada não sendo possivel enviar as demais solicitações
+    //res.send("Outra resposta")
 });
 
+app.get("/blog", function (req, res){
+    res.send("Bem vindo ao meu blog!");
+})
+
+app.get("/canal/youtube", function(req,res){
+    res.send("<h1>Bem vindo ao meu canal!</h1>");
+})
 
 app.listen(4000,function(erro){
     if(erro){
