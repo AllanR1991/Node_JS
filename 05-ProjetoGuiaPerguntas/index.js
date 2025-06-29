ec3e206e4078b777d8b7c6f32279081dab2eeb36
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-28 23:59:51 -0300

    feat: criado base do nosso servidor com express e configurado o express para utilizar como moto de visulização o EJS

---
+++ 05-ProjetoGuiaPerguntas/index.js
@@ -0,0 +1,16 @@
+//Importando o express
+const express = require("express")
+//Criando uma instancia do express
+const app = express();
+const porta = 8080;
+
+//Estou dizendo ao express para usar o EJS como view engine
+app.set('view engine','ejs')
+
+app.get("/",(req,res) => {
+    res.send("Bem vindo ao meu Projeto!");
+});
+
+app.listen(porta,()=>{
+    console.log(`App rodando! http://localhost:${porta}`);
+});
