acc1ddeed3dc411ed719db5178f1a7c406229fb2
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-06-30 23:29:05 -0300

    chore: resetar estrutura do projeto

--- 05-ProjetoGuiaPerguntas/index.js
+++ 05-ProjetoGuiaPerguntas/index.js
@@ -8,40 +8,9 @@ const porta = 8080;
 app.set('view engine','ejs')
 app.use(express.static('public')); //Configurando a aplicação para carregar arquivos staticos, imagem, css.
 
-app.get("/:nome/:lang",(req,res) => {
-    //res.send("Bem vindo ao meu Projeto!");
-
-
-    var nome = req.params.nome; //Pegando os paramentos informados pelo cliente e atribuido os valores.
-    var lang = req.params.lang;
-
-    var exibeMsg = true;
-
-    var produtos = [
-        {
-            nome: "Doritos",
-            preco: 3.14
-        },
-        {
-            nome: "Coca-Cola",
-            preco: 8.99
-        },
-        {
-            nome: "Leite",
-            preco: 5.56
-        },
-    ]
-
-
-    //Não é necessrio passar a rota pois ao usar res.render ele ja olha obrigatóriamente para a pasta views
-    res.render("index", {
-        nome,
-        lang,
-        empresa: "Guia do Programador",
-        incritos: 8000,
-        exibeMsg,
-        produtos
-    })
+app.get("/",(req,res) => {
+    
+    res.render("index")
 });
 
 app.listen(porta,()=>{
