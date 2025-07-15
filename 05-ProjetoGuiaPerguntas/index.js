5186e8a9df835362bd5c1da7e72673669fe2958f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-07-14 22:19:03 -0300

    feat: configurado body-parse e testado.

--- 05-ProjetoGuiaPerguntas/index.js
+++ 05-ProjetoGuiaPerguntas/index.js
@@ -4,9 +4,32 @@ const express = require("express")
 const app = express();
 const porta = 8080;
 
-//Estou dizendo ao express para usar o EJS como view engine
+const bodyParser = require("body-parser");
+// Importando o body-parser, que é um middleware para tratar os dados enviados no corpo (body) da requisição HTTP (ex: de um formulário).
+
+
 app.set('view engine','ejs')
-app.use(express.static('public')); //Configurando a aplicação para carregar arquivos staticos, imagem, css.
+// Estou dizendo ao express para usar o EJS como view engine
+
+app.use(express.static('public')); 
+// Configurando a aplicação para servir arquivos estáticos (como imagens, CSS, JS) da pasta "public".
+
+app.use(bodyParser.urlencoded({extended:false}))
+/* 
+  Middleware para ler dados enviados de formulários (method="POST").
+  - extended: false => permite apenas objetos simples (sem aninhamento de dados).
+  Ex: nome=Allan&idade=25 será convertido em { nome: 'Allan', idade: '25' }
+*/
+app.use(bodyParser.json());
+/*
+  Middleware para ler dados enviados em JSON no corpo da requisição (geralmente via JavaScript/AJAX).
+  Ex: { "nome": "Allan", "idade": 25 }
+*/
+
+/*
+Middleware = "intermediário" entre o pedido do cliente e a resposta do servidor.
+Ele processa, filtra, valida, ou transforma os dados durante esse caminho.
+*/
 
 app.get("/",(req,res) => {
     
@@ -18,7 +41,10 @@ app.get("/perguntar",(req, res)=>{
 })
 
 app.post("/salvarpergunta", (req,res)=>{
-    res.send("Formulario recebido.")
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+
+    res.send(`Formulario recebido. ${titulo} ${descricao}`)
 })
 
 app.listen(porta,()=>{
